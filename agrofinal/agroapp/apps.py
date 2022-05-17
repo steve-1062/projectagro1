51ff859f5bf3c5303c2d969b358b9e1d9847cdb0
= <=>
2022-05-17 10:40:13 +0530

    agro

---
+++ agrofinal/agroapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AgroappConfig(AppConfig):
+    name = 'agroapp'
---
+++ agrofinal/agroapp/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/CustomerProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name
+
+class Farmer(models.Model):
+    farmer=models.OneToOneField(User,on_delete=models.CASCADE)
+    aadhar_no=models.CharField(max_length=12)
+    phone_no=models.CharField(max_length=10)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    @property
+    def get_name(self):
+        return self.farmer.first_name+" "+self.farmer.last_name
+    @property
+    def get_id(self):
+        return self.farmer.id
+    def __str__(self):
+        return self.farmer.first_name
+
+
+
+class Product(models.Model):
+    # farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE)
+    name=models.CharField(max_length=40)
+    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    price = models.PositiveIntegerField()
+    description=models.CharField(max_length=40)
+    def __str__(self):
+        return self.name
+
+
+class Orders(models.Model):
+    STATUS =(
+        ('Pending','Pending'),
+        ('Order Confirmed','Order Confirmed'),
+        ('Out for Delivery','Out for Delivery'),
+        ('Delivered','Delivered'),
+    )
+    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    product=models.ForeignKey('Product',on_delete=models.CASCADE,null=True)
+    email = models.CharField(max_length=50,null=True)
+    address = models.CharField(max_length=500,null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    order_date= models.DateField(auto_now_add=True,null=True)
+    status=models.CharField(max_length=50,null=True,choices=STATUS)
+
+
+class Feedback(models.Model):
+    name=models.CharField(max_length=40)
+    feedback=models.CharField(max_length=500)
+    date= models.DateField(auto_now_add=True,null=True)
+    def __str__(self):
+        return self.name
